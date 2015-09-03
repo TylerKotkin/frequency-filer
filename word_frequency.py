684111ccec024dbc1877ff50c5943e094dad02c1
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-03 18:49:08 -0400

    progress

--- word_frequency.py
+++ word_frequency.py
@@ -1,2 +1,26 @@
+import re
+
 def word_frequency(text):
-    return {"hello": 1}
+    # return {"hello": 1}
+    punc_removed = re.sub(r'[,.!-]', "", sample).lower()
+    words = punc_removed.split()
+    how_many = {}
+    for i in words:
+        if i in how_many:
+            how_many[i] += 1
+        else:
+            how_many[i] = 1
+    return how_many
+
+def main():
+    sample = open('sample.txt')
+    text = sample.read()
+    how_many = word_frequency(text)
+    how_many = sorted(how_many.items(), key=lambda c: c[1], reverse=True)
+    how_many = how_many[:20]
+    for pair in how_many:
+        print(pair)
+    text.close()
+
+if __name__ == '__main__':
+    main()
